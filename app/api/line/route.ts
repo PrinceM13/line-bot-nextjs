f17261a7f99c9518e66a0ad16f2270ec51a8c494
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-04-28 16:23:43 +0700

    log(body): check message from body

--- app/api/line/route.ts
+++ app/api/line/route.ts
@@ -6,5 +6,8 @@ export async function POST(request: Request) {
   // get body
   const body = await request.json();
   console.log("check body ---> ", body);
+  if (body.events[0].type === "message") {
+    console.log("check body ---> ", body.events[0].message[0]);
+  }
   return new Response("Hello, INK13 POST !");
 }
