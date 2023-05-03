652869baa6c7bf0ec767e952d99df83bf314e146
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-05-03 13:57:20 +0700

    feat(flex): try flex msg

--- utils/line/reply.tsx
+++ utils/line/reply.tsx
@@ -16,6 +16,27 @@ export default async function reply(replyToken: string, msg: string) {
       {
         type: "text",
         text: `Do you need help on ${msg} ?`
+      },
+      {
+        type: "flex",
+        altText: "this is a flex message",
+        contents: {
+          type: "bubble",
+          body: {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "text",
+                text: "hello"
+              },
+              {
+                type: "text",
+                text: "world"
+              }
+            ]
+          }
+        }
       }
     ]
   };
