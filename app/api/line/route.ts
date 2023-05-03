3f8f9caf986506208dffd1a6ce8551d8cab320c2
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-05-03 16:02:05 +0700

    feat(call): show reply just when ink13Help was called

--- app/api/line/route.ts
+++ app/api/line/route.ts
@@ -11,12 +11,12 @@ export async function POST(request: Request) {
 
   const evnet = body.events[0];
   const replyToken = evnet.replyToken;
-  if (evnet.type === "message") {
+  console.log("check msg --> ", evnet.message);
+  console.log("check text --> ", evnet.message.text);
+  if (evnet.type === "message" && evnet.message.text === "ink13Help") {
     // console.log("check msg ---> ", body.events[0].message);
     // if (true)
     // if (evnet.message.text === "ink13Help")
-    // console.log("check msg --> ", evnet.message);
-    // console.log("check text --> ", evnet.message.text);
     await reply(replyToken, evnet.message.text);
     console.log("done");
   }
