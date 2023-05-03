52f950dfc0b1c30bebe36057932c5e76cddf05c3
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2023-05-03 15:06:11 +0700

    fix: remove some feature

--- utils/line/reply.tsx
+++ utils/line/reply.tsx
@@ -71,41 +71,41 @@ export default async function reply(replyToken: string, msg: string) {
       //     ]
       //   }
       // },
-      {
-        type: "template",
-        altText: "This is a buttons template",
-        template: {
-          type: "buttons",
-          thumbnailImageUrl: "https://example.com/bot/images/image.jpg",
-          imageAspectRatio: "rectangle",
-          imageSize: "cover",
-          imageBackgroundColor: "#FFFFFF",
-          title: "Menu",
-          text: "Please select",
-          defaultAction: {
-            type: "uri",
-            label: "View detail",
-            uri: "http://example.com/page/123"
-          },
-          actions: [
-            {
-              type: "postback",
-              label: "Buy",
-              data: "action=buy&itemid=123"
-            },
-            {
-              type: "postback",
-              label: "Add to cart",
-              data: "action=add&itemid=123"
-            },
-            {
-              type: "uri",
-              label: "View detail",
-              uri: "http://example.com/page/123"
-            }
-          ]
-        }
-      },
+      // {
+      //   type: "template",
+      //   altText: "This is a buttons template",
+      //   template: {
+      //     type: "buttons",
+      //     thumbnailImageUrl: "https://example.com/bot/images/image.jpg",
+      //     imageAspectRatio: "rectangle",
+      //     imageSize: "cover",
+      //     imageBackgroundColor: "#FFFFFF",
+      //     title: "Menu",
+      //     text: "Please select",
+      //     defaultAction: {
+      //       type: "uri",
+      //       label: "View detail",
+      //       uri: "http://example.com/page/123"
+      //     },
+      //     actions: [
+      //       {
+      //         type: "postback",
+      //         label: "Buy",
+      //         data: "action=buy&itemid=123"
+      //       },
+      //       {
+      //         type: "postback",
+      //         label: "Add to cart",
+      //         data: "action=add&itemid=123"
+      //       },
+      //       {
+      //         type: "uri",
+      //         label: "View detail",
+      //         uri: "http://example.com/page/123"
+      //       }
+      //     ]
+      //   }
+      // },
       {
         type: "text", // ①
         text: "Select your favorite food category or send me your location!",
@@ -139,12 +139,12 @@ export default async function reply(replyToken: string, msg: string) {
             }
           ]
         }
-      },
-      {
-        type: "richmenuswitch",
-        richMenuAliasId: "richmenu-alias-b",
-        data: "richmenu-changed-to-b"
       }
+      // {
+      //   type: "richmenuswitch",
+      //   richMenuAliasId: "richmenu-alias-b",
+      //   data: "richmenu-changed-to-b"
+      // }
     ]
   };
   await axios.post("https://api.line.me/v2/bot/message/reply", body, config);
